1156bbcc6899a92864e4cf785498a48216f372f6
PoulamiB <poulami.biswas@imaginea.com>
2015-03-12 17:44:46 +0530

    Created Directory Structure for downloaded emails

--- src/main/java/com/training/webcrawler/CrawlerUtil.java
+++ src/main/java/com/training/webcrawler/CrawlerUtil.java
@@ -5,61 +5,102 @@ import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.LinkedList;
-import java.util.ListIterator;
 
-public class CrawlerUtil {
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+public class CrawlerUtil
+{
 	/**
-	 *  The static variable emailsList is used to maintain the list of
-	 *  emails that are required to be downloaded from the given web page
+	 * the static variable dirpath is to maintain the location where the email's
+	 * are to be downloaded
 	 */
-	public static LinkedList<String> emailsList = new LinkedList<String>();
+	private static String dirPath = new String();
+
 	/**
+	 * the static variable fileInc is a sequence that has been used in file
+	 * naming
+	 */
+	private static int fileInc = 0;
+
+	private static String folderName = "";
+
+	public static String getDirPath()
+	{
+		return dirPath;
+	}
+
+	public static void setDirPath(String dirPath)
+	{
+		CrawlerUtil.dirPath = dirPath;
+	}
+
+	public static int getFileInc()
+	{
+		return fileInc;
+	}
+
+	public static void setFileInc(int fileInc)
+	{
+		CrawlerUtil.fileInc = fileInc;
+	}
+
+	public static String getFolderName()
+	{
+		return folderName;
+	}
+
+	public static void setFolderName(String folderName)
+	{
+		CrawlerUtil.folderName = folderName;
+	}
+
+	/**
+	 * This method checks if the link has already been parsed.
 	 * 
-	 * @param link This parameter will be checked against the list of
-	 * links to see if it already exists in the links
-	 * @param links This maintains the list of links that has already been 
-	 * parsed by the web crawler
+	 * @param link
+	 *            This parameter will be checked against the list of links to
+	 *            see if it already exists in the links
+	 * @param links
+	 *            This maintains the list of links that has already been parsed
+	 *            by the web crawler
 	 * @return
 	 */
-	public static int checkIfExists(String link, LinkedList<String> links) {
+	public static int checkIfExists(String link, LinkedList<String> links)
+	{
 		int index = 0;
 		index = links.indexOf(link);
 		return index;
 	}
-	
-	/**
-	 * This method adds the email content to a list of emails 
-	 * @param str This parameter contains the email body
-	 */
-	public static void addToEmailList(String str) {
-		emailsList.add(str);
-	}
 
 	/**
-	 * This method will store all the emails contained in the emailList
-	 * into a file
+	 * This method stores a single email in a file specified by the path
+	 * CrawlerUtil.getDirPath()
+	 * 
+	 * @param str
+	 * @throws IOException
 	 */
-	public static void storeEmailsInFile() throws IOException{
-		FileWriter fw ;
+	public static void storeEmailsInFile(String str) throws IOException
+	{
+		FileWriter fw;
 		BufferedWriter bw;
-		
-		File file = new File(
-				"/home/poulamib/workspace/WebCrawlerTest/emails.txt");
-		if (!file.exists()) {
+		// System.out.println(CrawlerUtil.getDirPath());
+		File file = new File(CrawlerUtil.getDirPath() + "/" + getFolderName()
+				+ "/email-" + (fileInc++) + ".txt");
+		if (!file.exists())
+		{
 			file.createNewFile();
 		}
-		
+
 		fw = new FileWriter(file.getAbsoluteFile());
 		bw = new BufferedWriter(fw);
-		try 
+		try
 		{
-			ListIterator<String> itr = emailsList.listIterator();
-			while (itr.hasNext()) {
-				bw.append(itr.next());
-			}
-			
-			System.out.println("Done");
-		} catch (IOException ex) {
+			bw.append(str);
+		}
+		catch (IOException ex)
+		{
 			ex.printStackTrace();
 			throw ex;
 		}
@@ -71,4 +112,137 @@ public class CrawlerUtil {
 
 	}
 
+	/**
+	 * This method will parse the web page based on the selector specified in
+	 * docSelector
+	 * 
+	 * @param doc
+	 *            The web page document that needs to be parsed
+	 * @param docSelector
+	 *            The selector on the basis of which the web page will be parsed
+	 * @param depth
+	 *            This parameter determines how deep the crawler has gone into
+	 *            the root web page
+	 * @throws IOException
+	 */
+	public static void parseUrl(Document doc, String docSelector, int depth)
+			throws IOException
+	{
+		Elements links = new Elements();
+		String[] selectors = docSelector.split("~");
+		for (String selector : selectors)
+		{
+			links = links.size() == 0 ? doc.select(selector) : links
+					.select(selector);
+		}
+		for (Element link : links)
+		{
+			if (depth == 0)
+			{
+				// CrawlerUtil.setDirPath(createDirectory(link.attr("abs:href")));
+				CrawlerUtil.createDirectory(link.attr("abs:href"));
+				CrawlerUtil.setFileInc(0); // if new month create a folder for
+											// that month
+			}
+			URLCrawler.processPage(link.attr("abs:href"), depth + 1);
+		}
+	}
+
+	/**
+	 * This method is used for parsing the web page that do not contain all the
+	 * links in a single page and also checks if that web page has been parsed
+	 * before
+	 * 
+	 * @param doc
+	 *            The web page document that needs to be parsed
+	 * @param depth
+	 *            The selector on the basis of which the web page will be parsed
+	 * @throws IOException
+	 */
+	public static void doPagination(Document doc, String docSelector, int depth)
+			throws IOException
+	{
+		LinkedList<String> absLinksList = new LinkedList<String>();
+		Elements links = doc.select(docSelector);
+		for (Element link : links)
+		{
+			if (CrawlerUtil.checkIfExists(link.attr("abs:href"), absLinksList) == -1)
+			{
+				URLCrawler.processPage(link.attr("abs:href"), depth + 1);
+				absLinksList.add(link.attr("abs:href"));
+			}
+
+		}
+	}
+
+	/**
+	 * 
+	 * @param doc
+	 * @throws IOException
+	 */
+	public static void downloadEmails(Document doc) throws IOException
+	{
+		Elements links = doc.getElementsByTag("pre");
+		for (Element link : links)
+		{
+			String str = link.toString().replaceAll("&gt;", "")
+					.replaceAll("&lt;", "");
+			storeEmailsInFile(str);
+		}
+	}
+
+	/**
+	 * To create a directory to store the emails
+	 * 
+	 * @param url
+	 * @return
+	 */
+	public static String createDirectory(String url)
+	{
+		setFolderName(getDirectoryName(url));
+		new File(getDirPath() + "/" + folderName).mkdir();
+		return getDirPath() + "/" + folderName;
+
+	}
+
+	/**
+	 * To get the directory name
+	 * 
+	 * @param url
+	 * @return
+	 */
+	public static String getDirectoryName(String url)
+	{
+		int firstIndex = url.indexOf("maven-users/") + "maven-users/".length();
+		int lastIndex = url.indexOf(".mbox");
+		return url.substring(firstIndex, lastIndex);
+	}
+
+	/**
+	 * 
+	 * @param directory
+	 * @return
+	 */
+	public static boolean deleteDirectory(File directory)
+	{
+		if (directory.exists())
+		{
+			File[] files = directory.listFiles();
+			if (null != files)
+			{
+				for (int i = 0; i < files.length; i++)
+				{
+					if (files[i].isDirectory())
+					{
+						deleteDirectory(files[i]);
+					}
+					else
+					{
+						files[i].delete();
+					}
+				}
+			}
+		}
+		return (directory.delete());
+	}
 }
--- src/main/java/com/training/webcrawler/URLCrawler.java
+++ src/main/java/com/training/webcrawler/URLCrawler.java
@@ -1,26 +1,39 @@
 package com.training.webcrawler;
 
+import java.io.File;
 import java.io.IOException;
-import java.util.LinkedList;
 
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
-import org.jsoup.nodes.Element;
-import org.jsoup.select.Elements;
 
-public class URLCrawler {
+public class URLCrawler
+{
 
 	/**
-	 * This is the main method which will start the web crawler and also store
-	 * the retrieved emails in a file.
+	 * This is the main method which will start the web crawler to download
+	 * emails from 2015 and save them in a directory where email's from each
+	 * month will be in a separate folder The main program deletes the
+	 * EmailDirectory if it exists and creates a new one with the same name
+	 * where the emails will be downloaded.
+	 * 
 	 * */
-	public static void main(String[] args) throws IOException {
+	public static void main(String[] args) throws IOException
+	{
+		System.out.println("******Web Crawler Started*******");
+		// com.training.webcrawler.URLCrawler
+		// "http://mail-archives.apache.org/mod_mbox/maven-users/"
+		CrawlerUtil.setDirPath(args[1]);
+		CrawlerUtil.deleteDirectory(new File(args[1])); // deleting the
+														// existing dir
+		new File(args[1]).mkdir();
+		// creating a new dir
+		processPage(args[0], 0);
 
-		processPage("http://mail-archives.apache.org/mod_mbox/maven-users/", 0);
-		CrawlerUtil.storeEmailsInFile();
+		System.out.println("*******Web Crawler Completed Successfully*******");
 	}
 
 	/**
+	 * This method is used for crawling the web page and parsing the urls
 	 * 
 	 * @param url
 	 *            - This is the web page that will be parsed to return the
@@ -29,67 +42,34 @@ public class URLCrawler {
 	 *            - This parameter determines how deep the crawler has gone into
 	 *            the root web page
 	 */
-	public static void processPage(String url, int depth) throws IOException {
+	public static void processPage(String url, int depth) throws IOException
+	{
 		Document doc = null;
-		try {
+		try
+		{
 			doc = Jsoup.connect(url).get();
-		} catch (IOException e1) {
+		}
+		catch (IOException e1)
+		{
 			e1.printStackTrace();
 			throw e1;
 		}
-
-		if (depth == 0) {
-			/*Elements links = doc.select("a[href^=201502]").select(
-					"a[href*=thread]");
-			for (Element link : links) {
-				processPage(link.attr("abs:href"), 1);
-			}
-*/		parseUrl(doc, "a[href^=201502]~a[href*=thread]", 0);
-		} else if (depth == 1) {
+		if (depth == 0)
+		{
+			CrawlerUtil.parseUrl(doc, "a[href^=2015]~a[href*=thread]", depth);
+		}
+		else if (depth == 1)
+		{
 			processPage(url, 2);
-			LinkedList<String> absLinksList = new LinkedList<String>();
-			Elements links = doc.select("a[href*=thread?]");
-			for (Element link : links) {
-				if (CrawlerUtil.checkIfExists(link.attr("abs:href"),
-						absLinksList) == -1) {
-					processPage(link.attr("abs:href"), 2);
-					absLinksList.add(link.attr("abs:href"));
-				}
-
-			}
-		} else if (depth == 2) {
-
-			Elements links = doc.select("a[href*=@]");
-			for (Element link : links) {
-				processPage(link.attr("abs:href"), 3);
-
-			}
-		} else if (depth == 3) {
-
-			Elements links = doc.getElementsByTag("pre");
-
-			for (Element link : links) {
-				String str = link.toString().replaceAll("&gt;", "")
-						.replaceAll("&lt;", "");
-				CrawlerUtil.addToEmailList(str);
-			}
-
+			CrawlerUtil.doPagination(doc, "a[href*=thread?]", depth);
 		}
-	}
-
-	public static void parseUrl(Document doc, String docSelector, int depth)
-			throws IOException {
-		Elements links = new Elements();
-		String[] selectors = docSelector.split("~");
-		for (String selector : selectors) {
-			if (links.size() == 0)
-				links = doc.select(selector);
-			else
-				links = links.select(selector);
-			links = links.size() == 0 ? doc.select(selector) : links.select(selector);
+		else if (depth == 2)
+		{
+			CrawlerUtil.parseUrl(doc, "a[href*=@]", depth);
 		}
-		for (Element link : links) {
-			processPage(link.attr("abs:href"), depth + 1);
+		else if (depth == 3)
+		{
+			CrawlerUtil.downloadEmails(doc);
 		}
 	}
 
